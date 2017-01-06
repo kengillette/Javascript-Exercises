f2775e814b2291c31d54c911182cab14ad9e91cc
Ken Gillette <ken@pocketprep.com>
2017-01-05 21:11:50 -0500

    Exercise 1

---
+++ Exercise 1/main.js
@@ -0,0 +1,17 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What value is stored in `givenName` right now?
+// A: The value is undefined
+console.log(givenName);
+
+givenName = "Brett";
+// Q: What is `givenName` set to now?
+// A: Now it's set to 'Brett'
+console.log(givenName);
+
+givenName = givenName;
+// Q: What is `givenName` set to now?
+// A: Still 'Brett'
+console.log(givenName);
